5f01073d377fe9bd3bf410197ab0901609bc0b93
= <=>
2025-11-05 20:51:53 -0300

    feat: Validando se a chave da API do Gemini está definida no .env e criando a primeira rota de teste para ver se está tudo funcionando

--- api/server.js
+++ api/server.js
@@ -8,3 +8,22 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+// Validação da API Key
+if (!process.env.GEMINI_API_KEY) {
+    console.error('ERRO: GEMINI_API_KEY não encontrada no arquivo .env');
+    process.exit(1);
+}
+
+
+// Rota de teste
+app.get('/', (req, res) => {
+    res.json({ 
+        mensagem: 'API funcionando!'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
